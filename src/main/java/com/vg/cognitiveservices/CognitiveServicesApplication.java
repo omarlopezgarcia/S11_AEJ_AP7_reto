cffa6b2ac206cfa542af5afecc70ec9abb6dec1b
MichaelJQC <michael.quispe@vallegrande.edu.pe>
2024-04-08 11:48:46 -0500

    Initial Project

---
+++ src/main/java/com/vg/cognitiveservices/CognitiveServicesApplication.java
@@ -0,0 +1,13 @@
+package com.vg.cognitiveservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CognitiveServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vg/cognitiveservices/CognitiveServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vg.cognitiveservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitiveServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
